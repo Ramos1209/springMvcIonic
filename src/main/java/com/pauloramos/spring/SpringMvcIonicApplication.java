889b046b25ec187a29da08fdc43d35be61634b6e
Paulo <paulo.1209@terra.com.br>
2018-01-17 23:02:23 -0200

    projeto criado

---
+++ src/main/java/com/pauloramos/spring/SpringMvcIonicApplication.java
@@ -0,0 +1,12 @@
+package com.pauloramos.spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcIonicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcIonicApplication.class, args);
+	}
+}
