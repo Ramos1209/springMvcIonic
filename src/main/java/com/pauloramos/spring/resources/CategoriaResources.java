858e45e12da502c8b3ef92191d8b7a06ed550525
Paulo <paulo.1209@terra.com.br>
2018-01-17 23:56:51 -0200

    Classes Categoria e CategoriaResourses

--- src/main/java/com/pauloramos/spring/resources/CategoriaResources.java
+++ src/main/java/com/pauloramos/spring/resources/CategoriaResources.java
@@ -1,17 +1,27 @@
 package com.pauloramos.spring.resources;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.request;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.pauloramos.spring.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResources {
 
 	@RequestMapping(method= RequestMethod.GET)
-	public String listar(){
-		return "Rest funcionando";
+	public List<Categoria> listar(){
+		
+		Categoria cat1 = new Categoria(1,"Informatica");
+		Categoria cat2 = new Categoria(2,"Camping");
+		
+		List<Categoria>lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		return lista;
 	}
 }
